21cc354ac5e3e3faba925ea6a30c06e4a8c5155a
J.B. Churchill <jchurchill@garrettcounty.org>
2014-10-07 16:09:16 -0400

    added YourFile.txt, file.php, javascript/local_pz.js, and local_pz.php and am writing data to the YourFile.txt file.

--- file.php
+++ file.php
@@ -1,10 +1,16 @@
 <?php
-  $File = "E:\map-data\Planning\Zoning\WEB_FILES\YourFile.txt";
+  if (isset($_POST[hidden_field])) {
+    $Data = trim(stripslashes($_POST[hidden_field]));
+  }
+?>  
+<?php
+  // $File = "E:\map-data\Planning\Zoning\WEB_FILES\YourFile.txt";
+  $File = "YourFile.txt"; // USE THIS ONE FOR LOCALHOST
   $Handle = fopen($File, 'a+');
-  $Data = "Jane Doe\r\n";
+  // $Data = "Jane Doe\r\n";
   fwrite($Handle, $Data);
-  $Data = "Bilbo Jones\r\n";
-  fwrite($Handle, $Data);
+  // $Data = "Bilbo Jones\r\n";
+  // fwrite($Handle, $Data);
   print "Data Added";
   fclose($Handle);
 ?>
